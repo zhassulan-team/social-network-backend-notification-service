d6c2e8940fafa4885e8ebae7daeb85777de3b5ea
Zhassulan Kaitanov <kaitanov.work@gmail.com>
2022-08-16 20:50:23 +0600

    init

---
+++ src/main/java/kata/academy/eurekanotificationservice/EurekaNotificationService.java
@@ -0,0 +1,14 @@
+package kata.academy.eurekanotificationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class EurekaNotificationService {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaNotificationService.class, args);
+    }
+}
