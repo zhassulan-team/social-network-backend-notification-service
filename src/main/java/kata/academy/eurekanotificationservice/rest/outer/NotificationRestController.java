d695a90f8682bb2ecbbd351462880d81ed9e9dec
OlyaBystrova <103462368+OlyaBystrova@users.noreply.github.com>
2022-11-01 18:41:46 +0300

    First Commit (#17)

--- src/main/java/kata/academy/eurekanotificationservice/rest/outer/NotificationRestController.java
+++ src/main/java/kata/academy/eurekanotificationservice/rest/outer/NotificationRestController.java
@@ -1,5 +1,9 @@
 package kata.academy.eurekanotificationservice.rest.outer;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import kata.academy.eurekanotificationservice.entity.Notification;
 import kata.academy.eurekanotificationservice.service.NotificationService;
 import lombok.RequiredArgsConstructor;
@@ -17,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.constraints.Positive;
 
+@Tag(name = "NotificationRestController", description = "Операции с нотификациями")
 @RequiredArgsConstructor
 @Validated
 @RestController
@@ -25,6 +30,10 @@ public class NotificationRestController {
 
     private final NotificationService notificationService;
 
+    @ApiOperation(value = "getNotificationPage", notes = "Получение страницы нотификаций пользователя")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Успешное получение страницы нотификаций пользователя"),
+            @ApiResponse(code = 400, message = "Ошибка при получении страницы нотификаций пользователя с указанным userId")})
     @GetMapping
     public ResponseEntity<Page<Notification>> getNotificationPage(@RequestParam(required = false) Boolean isViewed,
                                                                   @RequestHeader @Positive Long userId,
@@ -32,12 +41,21 @@ public class NotificationRestController {
         return ResponseEntity.ok(notificationService.findAllByIsViewedAndRecipientId(isViewed, userId, pageable));
     }
 
+    @ApiOperation(value = "viewAllNotification", notes = "Просмотр всех нотификаций пользователя")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Успешный просмотр всех нотификаций пользователя"),
+            @ApiResponse(code = 400, message = "Нотификаций, принадлежащих пользователю с указанным userId, нет в базе данных")})
     @PutMapping
     public ResponseEntity<Void> viewAllNotifications(@RequestHeader @Positive Long userId) {
         notificationService.viewAllNotifications(userId);
         return ResponseEntity.ok().build();
     }
 
+    @ApiOperation(value = "viewNotification", notes = "Просмотр нотификации пользователя")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Успешный просмотр нотификации пользователя"),
+            @ApiResponse(code = 400, message = "Нотификации с указанным notificationId, " +
+                    "принадлежащей пользователю с указанным userId, нет в базе данных")})
     @PutMapping("/{notificationId}")
     public ResponseEntity<Void> viewNotification(@PathVariable @Positive Long notificationId,
                                                  @RequestHeader @Positive Long userId) {
